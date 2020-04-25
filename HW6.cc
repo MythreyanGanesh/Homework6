4a8f17b9fc16c990ef130d35b049acfe752a5813
MythreyanGanesh <mythugan@gmail.com>
2020-04-24 19:09:37 -0500

    Header records displayed

--- HW6.cc
+++ HW6.cc
@@ -1,14 +1,34 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <stdint.h>
+#include <cstring>
 #include "cdk.h"
 
-
 #define MATRIX_WIDTH 3
-#define MATRIX_HEIGHT 3
-#define BOX_WIDTH 15
-#define MATRIX_NAME_STRING "Test Matrix"
+#define MATRIX_HEIGHT 5
+#define BOX_WIDTH 20
+#define MATRIX_NAME_STRING "Binary File Reading"
 
 using namespace std;
 
+
+class BinaryFileHeader{
+
+public:
+  uint32_t magicNumber;
+  uint32_t versionNumber;
+  uint64_t numRecords;
+};
+
+//records in the file have a fixed length buffer that will hold a c_style string
+const int maxRecordStringLength = 25;
+class BinaryFileRecord{
+
+public:
+  uint8_t strLength;
+  char stringBuffer[maxRecordStringLength];
+};
 
 int main()
 {
@@ -17,7 +37,7 @@ int main()
   CDKSCREEN     *cdkscreen;
   CDKMATRIX     *myMatrix;           // CDK Screen Matrix
 
-  const char            *rowTitles[MATRIX_HEIGHT+1] = {"R0", "R1", "R2", "R3"};
+  const char            *rowTitles[MATRIX_HEIGHT+1] = {"R0", "R1", "R2", "R3", "R4", "R5"};
   const char            *columnTitles[MATRIX_WIDTH+1] = {"C0", "C1", "C2", "C3"};
   int           boxWidths[MATRIX_WIDTH+1] = {BOX_WIDTH, BOX_WIDTH, BOX_WIDTH, BOX_WIDTH};
   int           boxTypes[MATRIX_WIDTH+1] = {vMIXED, vMIXED, vMIXED, vMIXED};
@@ -36,8 +56,8 @@ int main()
   /*
    * Create the matrix.  Need to manually cast (const char**) to (char **)
    */
-  myMatrix = newCDKMatrix(cdkscreen, CENTER, CENTER, MATRIX_WIDTH, MATRIX_HEIGHT, MATRIX_WIDTH, MATRIX_HEIGHT,
-                          MATRIX_NAME_STRING, (char **) columnTitles, (char **) rowTitles, boxWidths,
+  myMatrix = newCDKMatrix(cdkscreen, CENTER, CENTER, MATRIX_HEIGHT, MATRIX_WIDTH, MATRIX_HEIGHT, MATRIX_WIDTH,
+                          MATRIX_NAME_STRING, (char **) rowTitles, (char **) columnTitles, boxWidths,
 			  boxTypes, 1, 1, ' ', ROW, true, true, false);
 
   if (myMatrix ==NULL)
@@ -54,10 +74,31 @@ int main()
    */
   setCDKMatrixCell(myMatrix, 2, 2, "Test Message");
   drawCDKMatrix(myMatrix, true);    /* required  */
-  /* so we can see results */
-  sleep (10);
+  
+  BinaryFileHeader *myHeader = new BinaryFileHeader();
+  ifstream binHeader("/scratch/perkins/cs3377.bin", ios::in | ios::binary);
+  binHeader.read((char *) myHeader, sizeof(BinaryFileHeader));
+  string output;
+  stringstream ss;
+  ss << "Magic: 0x" << hex << myHeader->magicNumber;
+  output = ss.str();
+  setCDKMatrixCell(myMatrix, 1, 1, output.c_str());
+  ss.str("");
+  ss.clear();
+  ss << "Version: " << myHeader->versionNumber;
+  output = ss.str();
+  setCDKMatrixCell(myMatrix, 1, 2, output.c_str());
+  ss.str("");
+  ss.clear();
+  ss << "NumRecords: " << myHeader->numRecords;
+  output = ss.str();
+  setCDKMatrixCell(myMatrix, 1 ,3, output.c_str());
+  
+  drawCDKMatrix(myMatrix, true);
+  int y;
 
-
-  // Cleanup screen
+  cin >> y;
+  //clean up screen
   endCDK();
+  return 0;
 }
